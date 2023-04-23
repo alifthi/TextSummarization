73b245480b15a012144a34d5dda698e72c08585b
alifthi <alifathi8008@gmail.com>
2023-04-23 11:59:32 +0430

    TextSummarization: remove last codes in Model.py and put it to the main file

--- Codes/Model.py
+++ Codes/Model.py
@@ -1,9 +1,4 @@
-from json import decoder
 import tensorflow as tf
-import numpy as np
-from utils import utils
-
-
 class Model():
     def __init__(self,inputLength,latentDim,encoderVocabSize,decoderVocabSize): 
         self.inputLengh = inputLength
@@ -12,7 +7,7 @@ class Model():
         self.decoderVocabSize = decoderVocabSize
         self.net = self.buildModel()
     def buildModel(self):
-        encoderInput = tf.keras.Input(shape=[self.inputLengh,])
+        encoderInput = tf.keras.Input(shape=[self.inputLengh])
         x = tf.keras.layers.Embedding(self.encoderVocabSize,
                                       self.latentDim,
                                       mask_zero=False)(encoderInput)
@@ -20,7 +15,7 @@ class Model():
         _,state = tf.keras.layers.GRU(self.latentDim,return_state = True)(x)
         encoder = tf.keras.Model(inputs = encoderInput,outputs = state)
         encoderOutput = encoder(encoderInput)
-        decoderInput = tf.keras.Input(shape=[None,])
+        decoderInput = tf.keras.Input(shape=[None])
         x = tf.keras.layers.Embedding(self.decoderVocabSize,self.latentDim,mask_zero=False)(decoderInput)
         x = tf.keras.layers.BatchNormalization()(x)
         x,_ = tf.keras.layers.GRU(self.latentDim,return_state = True,
@@ -32,18 +27,8 @@ class Model():
     def compileModel(self,opt = tf.keras.optimizers.Adam(learning_rate = 0.001),loss = tf.keras.metrics.sparse_categorical_crossentropy):
         self.net.summary()
         self.net.compile(optimizer = opt,loss = loss)
-    def train(self,encoderInput,decoderInput,decoderTarget,epochs = 5,batchSize = 128):
+    def train(self,encoderInput,decoderInput,decoderTarget,epochs = 1,batchSize = 16):
         self.net.fit([encoderInput,decoderInput],decoderTarget,epochs = epochs,batch_size=batchSize)
     def saveModdel(self,addr='./Model'):
-        model.save(self.net,addr+'/modelstruct.h5')
-        model.saveweights(self.net,addr+'/modelweights.h5')
-utils = utils('/home/alifathi/Documents/AI/DATA/NLP/cnn_dailymail')
-utils.describeTrainData()
-[corpus,summary,encoderVocabSize,decoderVocabSize] = utils.preprocessing(utils.trainData)
-decoderInputData = summary[:,:-1]
-decoderTarget = summary[:,1:]
-model = Model(inputLength=corpus.shape[1],latentDim=100,
-              encoderVocabSize = encoderVocabSize,
-              decoderVocabSize=decoderVocabSize)
-model.compileModel()
-model.train(encoderInput=corpus,decoderInput=decoderInputData,decoderTarget=decoderTarget)
+        self.net.save(self.net,addr+'/modelstruct.h5')
+        self.net.saveweights(self.net,addr+'/modelweights.h5')
